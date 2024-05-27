6764f1ab9869d5f52c71adc96e0ee1e3a9d9194e
jcg0 <jg.awsprod@gmail.com>
2024-05-27 11:39:08 -0400

    finished python project2

---
+++ project2.py
@@ -0,0 +1,24 @@
+name = "Jefferson Griebel"
+major = "Programming & Analysis"
+answer = "I want to have a deeper\nunderstanding about the right way to code."
+num1 = 49
+num2 = 31
+addition = num1+num2
+subtraction = num1-num2
+multiply = num1*num2
+divide = num1/num2
+modulo = num1 % num2
+print(
+    f"My name is {name}. My major is {major}.\nI'm most interested in this class because {answer}")
+
+print(f"{num1} + {num2} = {addition}")
+
+print(f"{num1} - {num2} = {subtraction}")
+
+print(f"{num1} * {num2} = {multiply}")
+
+print(f"{num1} / {num2} = {divide}")
+
+print(f"{num1} % {num2} = {modulo}")
+
+print("This is the end of the program!")
