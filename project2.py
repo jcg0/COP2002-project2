726dcbc648d44f0729698c7a0d8fbace80b259bd
jcg0 <jg.awsprod@gmail.com>
2024-05-27 13:10:39 -0400

    Finished python project2!

--- project2.py
+++ project2.py
@@ -3,21 +3,23 @@ major = "My major is Programming & Analysis."
 answer = "\nI'm most interested in this class because I want to have a deeper\nunderstanding about the right way to code."
 num1 = 49
 num2 = 31
-addition = f" = {num1 + num2}"
-subtraction = f" = {num1 - num2}"
-multiply = f" = {num1 * num2}"
-divide = f" = {num1 / num2}"
-modulo = f" = {num1 % num2}"
+addition = num1+num2
+subtraction = num1-num2
+multiply = num1*num2
+divide = num1/num2
+modulo = num1 % num2
+
 print(name + major + answer)
-
-print(f"{num1} + {num2}{addition}")
-
-print(f"{num1} - {num2}{subtraction}")
-
-print(f"{num1} * {num2}{multiply}")
-
-print(f"{num1} / {num2}{divide}")
-
-print(f"{num1} % {num2}{modulo}")
-
+print()
+print(f"{num1} + {num2} = {addition}")
+print()
+print(f"{num1} - {num2} = {subtraction}")
+print()
+print(f"{num1} * {num2} = {multiply}")
+print()
+print(f"{num1} / {num2} = {divide}")
+print()
+print(f"{num1} % {num2} = {modulo}")
+print()
+print()
 print("This is the end of the program!")
